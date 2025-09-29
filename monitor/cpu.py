876df4d717752ddae441ed1a42b70daee9c58ada
Javier <161946280+javiervilchezl@users.noreply.github.com>
2025-09-29 07:46:43 +0200

    Delete monitor directory

--- monitor/cpu.py
+++
@@ -1,5 +0,0 @@
-import psutil
-
-def get_cpu_usage():
-    """Devuelve el uso actual de CPU en porcentaje."""
-    return psutil.cpu_percent(interval=1)
--- monitor/memory.py
+++
@@ -1,5 +0,0 @@
-import psutil
-
-def get_memory_usage():
-    """Devuelve el porcentaje de uso de RAM."""
-    return psutil.virtual_memory().percent
